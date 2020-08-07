574481b61857cb1045775ac02353b64d094cadae
Rob Young <young@ecohealthalliance.org>
2020-08-07 10:44:40 -0400

    turned off debug mode
    shouldn't commit setting this to True!!

--- wabnet/settings.py
+++ wabnet/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-#DEBUG = os.environ.get('DEBUG', False)
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = ['*']
 
